76d12ad722f638eaf5154402a2f8b7ac07e93bd9
翁悦俊(wengyj) <wengyj@wifi.com>
2020-07-12 00:11:47 +0800

    1，case中 使用已有的计算器相关方法

--- py/case/conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-
-@pytest.fixture(scope='function', )
-def cacl_up_down():
-    print('【开始计算】')
-    yield
-    print("【结束计算】")
--- py_code/case/test_demo.py
+++ py_code/case/test_demo.py
@@ -1,9 +1,11 @@
 import pytest
+from py_code import cacl
 # -*- coding： utf-8 -*-
 import yaml
 
 
 class Test_Demo:
+    res = cacl.Cacl()
 
     @pytest.mark.parametrize(('a', 'b', 'c'), yaml.safe_load(open(r'../data/data.yaml'))['add'])
     def test_add(self, cacl_up_down, a, b, c):
@@ -13,8 +15,9 @@ class Test_Demo:
         :param b:
         :return:
         """
+        res = cacl.Cacl()
         print(f'加法{a} + {b}=={c}')
-        assert c == a + b
+        assert c == self.res.add(a, b)
 
     @pytest.mark.parametrize(('a', 'b', 'c'), yaml.safe_load(open(r'../data/data.yaml'))['minus'])
     def test_minus(self, cacl_up_down, a, b, c):
@@ -25,7 +28,7 @@ class Test_Demo:
         :return:
         """
         print(f'减法{a} - {b}=={c}')
-        assert c == a - b
+        assert c == self.res.minus(a, b)
 
     @pytest.mark.parametrize(('a', 'b', 'c'), yaml.safe_load(open(r'../data/data.yaml'))['product'])
     def test_product(self, cacl_up_down, a, b, c):
@@ -36,7 +39,7 @@ class Test_Demo:
         :return:
         """
         print(f'乘法{a} * {b}=={c}')
-        assert c == a * b
+        assert c == self.res.product(a, b)
 
     @pytest.mark.parametrize(('a', 'b', 'c'), yaml.safe_load(open(r'../data/data.yaml'))['div'])
     def test_div(self, cacl_up_down, a, b, c):
@@ -47,7 +50,7 @@ class Test_Demo:
         :return:
         """
         print(f'除法{a} / {b}=={c}')
-        assert c == a / b
+        assert c == self.res.div(a, b)
 
 
 if __name__ == '__main__':
